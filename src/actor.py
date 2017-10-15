d5d3c4404bc8e24315870e5b2231a8d7f9c12c53
Richard Belleville <j.belleville.richard@gmail.com>
2017-10-15 08:51:48 -0500

    Add test for ask. Fix ask implementation

--- src/actor.py
+++ src/actor.py
@@ -1,38 +1,24 @@
 from concurrent import futures
 from concurrent.futures import Future
-import asyncio
+import threading
 
 class Actor(object):
     def __init__(self, parent: 'Actor', executor: futures.Executor = None) -> None:
         self._executor = parent._executor if executor is None else executor
         self._parent = parent
         self._children = []
-        self._resp_event = None
+        self._resp_cond = None
         self._resp = None
 
     def send(self, message: 'Message', recipient: 'Actor') -> None:
-        recipient.receive(message, self)
-
-    def respond(self, message: 'Message', recipient: 'Actor') -> None:
-        recipient.__receive_response(message, self)
-
-    def __receive_response(self, message: 'Message', responder: 'Actor') -> None:
-        assert self._resp_event is not None
-        self._resp = message
-        self._resp_event.set()
+        return recipient.receive(message, self)
 
     def receive(self, message: 'Message', sender: 'Actor') -> None:
         raise Exception("Abstract method! What the hell are you doing?")
 
     def ask(self, message: 'Message', recipient: 'Actor') -> 'Future[Message]':
         def _ask():
-            assert self._resp_event is None
-            self._resp_event = asyncio.Event()
-            self.send(message, recipient)
-            self._resp_event.wait()
-            resp = self._resp
-            self._resp = None
-            return resp
+            return self.send(message, recipient)
         return self._executor.submit(_ask)
 
     def parent(self) -> 'Actor':
--- test/test_actor.py
+++ test/test_actor.py
@@ -2,16 +2,52 @@ from actor import Actor
 from concurrent.futures import ThreadPoolExecutor
 import pytest
 
-class EchoActor(Actor):
+class PrintActor(Actor):
     def receive(self, message, sender):
         print(message)
+
+class PassAlongActor(Actor):
+    def __init__(self, target, parent, executor):
+        self._target = target
+        super().__init__(parent, executor)
+
+    def receive(self, message, sender):
+        self.send(message, self._target)
+
+class EchoActor(Actor):
+    def receive(self, message, sender):
+        return message
+
+class AskActor(Actor):
+    def __init__(self, target, parent, executor):
+        self._target = target
+        self_message = None
+        super().__init__(parent, executor)
+
+    def receive(self, message, sender):
+        resp = self.ask(message, self._target)
+        self._message = resp.result()
 
 @pytest.fixture(scope="module")
 def test_executor():
     return ThreadPoolExecutor(max_workers=4)
 
 def test_send(test_executor, capsys):
-    a = EchoActor(None, executor=test_executor)
+    a = PrintActor(None, executor=test_executor)
     a.receive("stuff", None)
-    out, err = capsys.readouterr()
+    out, _ = capsys.readouterr()
     assert out == "stuff\n"
+
+def test_send_actor_to_actor(test_executor, capsys):
+    print_actor = PrintActor(None, executor=test_executor)
+    pass_along = PassAlongActor(print_actor, None, executor=test_executor)
+    pass_along.receive("hello", None)
+    out, _ = capsys.readouterr()
+    assert out == "hello\n"
+
+def test_ask(test_executor):
+    for i in range(10000):
+        echo_actor = EchoActor(None, executor=test_executor)
+        ask_actor = AskActor(echo_actor, None, executor=test_executor)
+        ask_actor.receive("test", None)
+        assert ask_actor._message == "test"
